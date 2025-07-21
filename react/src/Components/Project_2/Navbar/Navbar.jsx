946c7584b3faecd84beb8e0e0494d9958de48c6b
Raj Singh <rajsinghdurgapur26@gmail.com>
2025-07-21 16:48:19 +0530

    changes

--- react/src/Components/Project_2/Navbar/Navbar.jsx
+++ react/src/Components/Project_2/Navbar/Navbar.jsx
@@ -1,12 +1,28 @@
 // Navbar.jsx
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => clearInterval(interval); // cleanup on unmount
+  }, []);
   return (
     <nav className="bg-blue-300 text-black p-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-2xl font-bold">Port-folio</div>
+        <h1
+          id="clock"
+          className=" font-mono text-indigo-800 px-6 py-4 border-2 rounded-xl shadow-lg tracking-widest text-center"
+        >
+          {time}
+        </h1>
+
         <ul className="flex space-x-6 text-lg">
           <li>
             <NavLink
